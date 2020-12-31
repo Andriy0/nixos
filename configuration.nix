d5d70ee70a8e2a256508cea0127a1782fdd231dd
Andriy0 <56863249+Andriy0@users.noreply.github.com>
2020-12-31 18:39:13 +0200

    backup before restructuring

--- configuration.nix
+++ configuration.nix
@@ -10,19 +10,20 @@
       ./hardware-configuration.nix
     ];
 
-  boot.loader = {
-      efi = {
-          # canTouchEfiVariables = true;
+  boot.loader = 
+    { efi = 
+        { # canTouchEfiVariables = true;
           efiSysMountPoint = "/boot/efi";
-      };
-      grub = {
-          efiSupport = true;
+        };
+      grub = 
+        { efiSupport = true;
           efiInstallAsRemovable = true;
           device = "nodev";
-      };
-  };
+        };
 
-  networking.hostName = "nixos"; # Define your hostname.
+    };
+
+  # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
@@ -31,10 +32,25 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp1s0.useDHCP = true;
+  # networking.useDHCP = false;
+  # networking.interfaces.enp1s0.useDHCP = true;
+  # networking.interfaces.wlo1.useDHCP = true;
 
-  networking.networkmanager.enable = true;
+  networking = 
+    { hostName = "nixos";
+      defaultGateway = "192.168.1.1";
+      nameservers = [ "8.8.8.8" ];
+      firewall.enable = true;
+      # networkmanager.enable = true;
+      wireless = 
+        { enable = true;
+          networks = 
+            { "UKrtelecom_JZJ87Q" = 
+                { pskRaw = "be9f9dc510f25edb3e12259a2eeb0432277e7cbb74b6b0d0677fde9c4ad0b78d";
+                };
+            };
+        };
+    };
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -48,50 +64,122 @@
   # };
   
   # X11
-  services.xserver = {
-      enable = true;
+  services.xserver = 
+    { enable = true;
       layout = "us,ua";
       xkbOptions = "caps:escape_shifted_capslock,altwin:menu_win,grp:alt_shift_toggle";
-      libinput.enable = true;
-      desktopManager.xterm.enable = false;
-      displayManager.lightdm.enable = true;
-      displayManager.defaultSession = "none+awesome";
-      # windowManager.i3.enable = true;
-      windowManager.awesome.enable = true;
+      videoDrivers = [ "modesetting" ];
+      useGlamor = true;
+      libinput = 
+        { enable = true;
+          naturalScrolling = true;
+          additionalOptions = 
+            '' Option "TappingButtonMap" "lmr"
+            '';
+        };
+      desktopManager = 
+        { xterm.enable = false;
+        };
+      displayManager = 
+        { lightdm.enable = true;
+          defaultSession = "none+awesome";
+        };
+      windowManager = 
+        { awesome.enable = true;
+        };
   };
 
+  services.picom = 
+    { enable = true;
+      fade = false;
+      inactiveOpacity = 1.0;
+      shadow = false;
+      backend = "glx";
+      vSync = true;
+      settings = 
+        { use-damage = true;
+          glx-no-stencil = true;
+          glx-no-rebind-pixmap = true;
+          xrendr-sync-fence = true;
+        };
+    };
+
+  services.cron = 
+    { enable = true;
+      systemCronJobs = 
+        [ "@reboot  root  sysctl dev.i915.perf_stream_paranoid=0"
+        ];
+    };
   # services.xserver.xkbOptions = "eurosign:e";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
-
+  
   # Enable sound.
   sound.enable = true;
   # hardware.pulseaudio.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users = {
-    andriy = {
-        isNormalUser = true;
-        home = "/home/andriy";
-	    description = "Andrii";
-	    extraGroups = [ "wheel" "audio" "video" ]; # Enable ‘sudo’ for the user.
-	};
-  };
+  users.groups = 
+    { andriy.gid = 1000;
+    };
 
-  nixpkgs.config = {
-	allowUnfree = true;
-  };
+  users.users = 
+    { andriy = 
+        { isNormalUser = true;
+          home = "/home/andriy";
+          description = "Andrii";
+          uid = 1000;
+          group = "andriy";
+          extraGroups = [ "users" "wheel" "audio" "video" "networkmanager" ];
+          shell = pkgs.fish;
+        };
+    };
+
+  programs.fish.enable = true;
+
+  boot.kernel.sysctl = 
+    { "vm.swappiness" = 10;
+      "dev.i915.perf_stream_paranoid" = 0;
+    };
+
+  nixpkgs.config.packageOverrides = pkgs: 
+    { vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+    };
+
+  hardware.opengl = 
+    { driSupport = true;
+      extraPackages = with pkgs; 
+        [ intel-compute-runtime
+          intel-media-driver
+          vaapiIntel
+          vaapiVdpau
+          libvdpau-va-gl
+        ];
+    };
+
+  nixpkgs.config = 
+    { allowUnfree = true;
+    };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    wget vim neovim git xclip brave alacritty nodejs 
-  ];
+  environment.systemPackages = with pkgs; 
+    [ wget vim neovim git xclip alacritty nodejs arandr xorg.xev acpi firefox nitrogen 
+      networkmanagerapplet volumeicon lxappearance arc-theme papirus-icon-theme pcmanfm
+      clinfo vulkan-tools light (chromium.override { enableVaapi = true; }) libva-utils
+      flameshot starship
+    ];
   
-  fonts.fonts = with pkgs; [
-    mononoki 
-  ];
+  fonts.fonts = with pkgs; 
+    [ (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "Mononoki" ]; })
+    ];
+
+  documentation = 
+    { enable = true;
+      man.enable = true;
+      dev.enable = true;
+    };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -100,7 +188,12 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-  security.doas.enable = true;
+  security.doas = 
+    { enable = true;
+      extraRules = 
+        [ { users = [ "andriy" ]; noPass = true; keepEnv = true; }
+        ];
+    };
 
   # List services that you want to enable:
 
--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -4,40 +4,62 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
-    ];
+  imports = [ ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+  boot.extraModprobeConfig = 
+    '' options snd_hda_intel index=1
+    '';
+  hardware.enableRedistributableFirmware = true;
+  hardware.enableAllFirmware = true;
+  
+  # Powersaving
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [ "intel_pstate=disable" ];
+  services.tlp = 
+    { enable = true;
+	  settings = 
+        { CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
+		  CPU_SCALING_GOVERNOR_ON_BAT = "ondemand";
+
+		  CPU_SCALING_MIN_FREQ_ON_AC = 500000;
+		  CPU_SCALING_MAX_FREQ_ON_AC = 1600000;
+		  CPU_SCALING_MIN_FREQ_ON_BAT = 500000;
+		  CPU_SCALING_MAX_FREQ_ON_BAT = 1600000;
+        };
+    };
+
+  # Filesystems
   fileSystems."/" =
-    { device = "/dev/disk/by-label/nixos";
+    { device = "/dev/sda6";
       fsType = "btrfs";
-      options = [ "rw,noatime,compress=zstd,space_cache,commit=120,subvol=@" ];
+      options = [ "rw,noatime,ssd,compress=zstd,space_cache,commit=120,subvol=@" ];
     };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-label/nixos";
+    { device = "/dev/sda6";
       fsType = "btrfs";
-      options = [ "rw,noatime,compress=zstd,space_cache,commit=120,subvol=@home" ];
+      options = [ "rw,noatime,ssd,compress=zstd,space_cache,commit=120,subvol=@home" ];
     };
 
   fileSystems."/.snapshots" =
-    { device = "/dev/disk/by-label/nixos";
+    { device = "/dev/sda6";
       fsType = "btrfs";
-      options = [ "rw,noatime,compress=zstd,space_cache,commit=120,subvol=@snapshots" ];
+      options = [ "rw,noatime,ssd,compress=zstd,space_cache,commit=120,subvol=@snapshots" ];
     };
 
   fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-label/ESP";
+    { device = "/dev/disk/by-uuid/061D-C556";
       fsType = "vfat";
       options = [ "rw,noatime" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-label/swap"; }
+    [ { device = "/dev/disk/by-uuid/ad752a2b-9336-4b90-b04f-b9e9db4001f8"; }
     ];
 
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
 }
